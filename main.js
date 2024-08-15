6b93fd9b0f8eb312bd22533f36da0843c932a5fd
Felpezs <felipe.edfreire@gmail.com>
2024-08-15 16:59:04 -0300

    cards

---
+++ main.js
@@ -0,0 +1,12 @@
+import "./style.scss"
+
+const featuresEl = document.querySelector(".features")
+const featureEls = document.querySelectorAll(".feature")
+
+featuresEl.addEventListener("pointermove", (ev)=>{
+  featureEls.forEach((featureEl)=>{
+    const rect = featureEl.getBoundingClientRect();
+    featureEl.style.setProperty("--x", ev.clientX - rect.left);
+    featureEl.style.setProperty("--y", ev.clientY - rect.top);
+  })
+})
